deb5c2de69dea84aed7e9bcae9c27391a86c3258
Kyo Lee <kyolee310@gmail.com>
2018-05-17 13:06:57 -0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,7 @@
 window.myConfig = window.myConfig || {};
 
+// This configuration will not be used when the application runs on Heroku.
 myConfig = {
-  omdb_apikey: '' // ex. 123abc456de
+  omdb_apikey: '' // ex: 123abc456de
 };
 
